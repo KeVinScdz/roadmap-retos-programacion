9ca7cd8581003f58fbfbf697abe9ac894877e541
KeVinScdz <cordobasantiagokevin@gmail.com>
2025-06-17 07:44:18 -0500

    KeVinScdz.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/KeVinScdz.py
@@ -0,0 +1,31 @@
+# https://python.org
+
+# Comentario en python una linea
+
+"""
+Comentario
+Multiples
+Lineas
+"""
+
+'''
+Esto es lo mismo
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo Valor"
+
+MY_CONSTANT = "Mi contstante" #Por convencion 
+
+my_int = 1 #Enteros
+my_float = 1.5 #Decimal
+my_bool = True
+my_bool = False
+my_string = "Cadena de texto"
+my_other_string = 'otro texto'
+
+print("Hola, python!")
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string)) 
